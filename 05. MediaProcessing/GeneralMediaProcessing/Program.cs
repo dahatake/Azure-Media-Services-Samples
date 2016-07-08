2cf06cdcb0e5b7925093fba389dd79203bc8d4dd
Daiyu Hatakeyama <dahatake@hotmail.com>
2016-07-08 19:07:14 +0900

    Adding Visual Studio Solutoin Files

---
+++ 05. MediaProcessing/GeneralMediaProcessing/Program.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.WindowsAzure.MediaServices.Client;
+using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace GeneralMediaProcessing
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var MPtype = ConfigurationManager.AppSettings["MPType"];
+			var targetFile = new FileInfo(ConfigurationManager.AppSettings["uploadfile"]);
+			var downloadLocation = new FileInfo(ConfigurationManager.AppSettings["downloadLocation"]);
+            var AssetID = ConfigurationManager.AppSettings["AssetID"];
+            var DoDownload = Boolean.Parse(new FileInfo(ConfigurationManager.AppSettings["DoDownload"]).ToString());
+
+			// 処理時間の計測
+			var totalSw = new Stopwatch();
+			totalSw.Start();
+
+			Console.WriteLine("***** 1. Azure メディアサービス 接続 *****");
+			var context = new CloudMediaContext(
+					ConfigurationManager.AppSettings["accountName"],
+					ConfigurationManager.AppSettings["accountKey"]
+					);
+
+            IAsset asset = null;
+            if (AssetID.Length > 0)
+            {
+                Console.WriteLine("***** 2. ファイルアップロード *****");
+                var queryData = from a in context.Assets
+                        where a.Id == AssetID
+                        select a;
+                try
+                {
+                    if (queryData.Count<IAsset>() == 0)
+                    {
+                        Console.WriteLine("**** 指定したアセットが見つかりません ****");
+                        Console.ReadLine();
+                        return;
+                    }
+                    else
+                    {
+                        asset = queryData.FirstOrDefault();
+                    }
+
+                } catch (Exception e)
+                {
+                    Console.WriteLine("**** 指定したアセットが見つかりません ****");
+                    Console.WriteLine($"**** [Error]{e.Message}");
+                    Console.ReadLine();
+                    return;
+
+                }
+
+            }
+            else
+            {
+
+                Console.WriteLine("***** 2. ファイルアップロード *****");
+                asset = context.Assets.CreateFromFile(
+                    targetFile.FullName,
+                    AssetCreationOptions.None,
+                    (a, p) =>
+                    {
+                        Console.WriteLine("  経過 {0}%", p.Progress);
+                    });
+
+            }
+			Console.WriteLine("***** 3. Media Processor 実行 *****");
+			var mediaProcessor = (from mp in context.MediaProcessors
+								  where mp.Name.Contains(MPtype)
+								  orderby mp.Name
+								  select mp
+								  ).First();
+			var configText = File.ReadAllText("config" + MPtype.Replace(" ","") + ".txt");
+
+			var job = context.Jobs.CreateWithSingleTask(
+				mediaProcessor.Name,
+				configText,
+				asset,
+				asset.Name + "-" + MPtype.Replace(" ", "") + "-Output",
+				AssetCreationOptions.None);
+			job.Submit();
+			job = job.StartExecutionProgressTask(j =>
+			{
+				Console.WriteLine("   状態: {0}", j.State);
+				Console.WriteLine("   経過: {0:0.##}%", j.GetOverallProgress());
+			},
+			System.Threading.CancellationToken.None).Result;
+
+            if (job.State != JobState.Finished)
+            {
+                Console.WriteLine($"Error: {job.Tasks[0].ErrorDetails[0].Message}");
+                Console.ReadLine();
+                return;
+            }
+			var outputAsset = job.OutputMediaAssets[0];
+
+			Console.WriteLine("***** 4. ダウンロード用設定 *****");
+
+			var locatorType = LocatorType.Sas;
+			if (MPtype.Equals("Encoder Standard")) {
+				locatorType = LocatorType.OnDemandOrigin;
+			}
+
+			context.Locators.CreateLocator(
+				locatorType,
+				outputAsset,
+				context.AccessPolicies.Create(
+					"Streaming Access Policy",
+					TimeSpan.FromDays(7),
+				AccessPermissions.Read)
+			);
+
+			if (DoDownload && !MPtype.Equals("Encoder Standard"))
+			{
+				Console.WriteLine("***** 5. ファイルダウンロード *****");
+				outputAsset.DownloadToFolder(downloadLocation.FullName);
+			}
+
+			Console.WriteLine("***** 処理終了 *****");
+			Console.WriteLine("総処理時間: {0}", totalSw.Elapsed.ToString());
+			Console.WriteLine("何かキーを押してください。");
+			Console.ReadLine();
+
+
+		}
+	}
+}
---
+++ 10. LiveAdmin/LiveAdmin/default.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="LiveAdmin._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Azure Media Services - ライブ配信モニタリング ツール</title>
+	<link href="//amp.azure.net/libs/amp/latest/skins/amp-default/azuremediaplayer.min.css" rel="stylesheet" />
+	<script src= "//amp.azure.net/libs/amp/latest/azuremediaplayer.min.js"></script>
+
+</head>
+<body style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif">
+    <form id="form1" runat="server">
+    <div>
+		<label id="status" />
+		<asp:Label ID="title" runat="server" Text="Azure Media Services - ライブモニタリングツール" style="font-size: xx-large"></asp:Label>
+		<asp:Label ID="lblMessage" runat="server"></asp:Label>
+		<br />
+		<asp:Table ID="tableListChannel" runat="server" 
+			BorderWidth="1px" BorderStyle="Solid" GridLines="Both"
+			width="100%">
+		</asp:Table>
+
+    </div>
+    </form>
+</body>
+</html>
